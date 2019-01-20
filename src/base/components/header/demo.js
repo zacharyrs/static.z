a74533f38bba725bcd98da938bb5d0cdcb6f55ae
Zachary Riedlshah <git@zacharyrs.me>
2019-01-20 17:22:17 +1300

    wip: attempting to get patternplate working

--- src/base/components/header/demo.js
+++ src/base/components/header/demo.js
@@ -5,6 +5,14 @@ import * as html from './header.html'
 import * as css from './header.css'
 
 export { html, css }
+
 export default () => {
   console.log('hi')
 }
+
+// Alternate approach
+// module.exports = {
+//   default: () => {},
+//   html: `<h1 class="hello-world">Hello World</h1>`,
+//   css: `.hello-world { font-family: sans-serif; color: cornflowerblue; }`,
+// }
