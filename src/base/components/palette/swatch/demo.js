858795ac4063e7e0eb5ce1f9d03f0307396761f0
Zachary Riedlshah <git@zacharyrs.me>
2019-02-16 00:33:46 +1300

    feat: added basic swatches using cards for demoing color palette

--- src/base/components/palette/swatch/demo.js
+++
@@ -1,4 +0,0 @@
-export const html = () => require('./swatch.html') // eslint-disable-line  import/no-unresolved
-export const css = () => require('./swatch.css') // eslint-disable-line  import/no-unresolved
-
-export default () => {}
